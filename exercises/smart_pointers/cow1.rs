abe6c94e45e616149163eb20a4ddeab28c5a427a
skgwd <skgwd2021@aliyun.com>
2025-05-02 18:21:07 +0800

    update 80

--- exercises/smart_pointers/cow1.rs
+++ exercises/smart_pointers/cow1.rs
@@ -12,8 +12,6 @@
 //
 // Execute `rustlings hint cow1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::borrow::Cow;
 
 fn abs_all<'a, 'b>(input: &'a mut Cow<'b, [i32]>) -> &'a mut Cow<'b, [i32]> {
@@ -21,9 +19,12 @@ fn abs_all<'a, 'b>(input: &'a mut Cow<'b, [i32]>) -> &'a mut Cow<'b, [i32]> {
         let v = input[i];
         if v < 0 {
             // Clones into a vector if not already owned.
+            // 如果 Cow 是 Borrowed，会克隆数据变为 Owned， 如果已经是 Owned，直接返回可变引用
+            // 即只有在Borrowed状态下且需要修改数据时才会clone
             input.to_mut()[i] = -v;
         }
     }
+
     input
 }
 
@@ -34,6 +35,7 @@ mod tests {
     #[test]
     fn reference_mutation() -> Result<(), &'static str> {
         // Clone occurs because `input` needs to be mutated.
+        // 因为有负数，会触发 to_mut() 克隆数据，预期结果应该是 Cow::Owned
         let slice = [-1, 0, 1];
         let mut input = Cow::from(&slice[..]);
         match abs_all(&mut input) {
@@ -45,10 +47,12 @@ mod tests {
     #[test]
     fn reference_no_mutation() -> Result<(), &'static str> {
         // No clone occurs because `input` doesn't need to be mutated.
+        // 没有负数，不会触发克隆(不会触发to_mut())，预期结果保持 Cow::Borrowed
         let slice = [0, 1, 2];
         let mut input = Cow::from(&slice[..]);
         match abs_all(&mut input) {
-            // TODO
+            Cow::Borrowed(_) => Ok(()),
+            _ => Err("Expected borrowed value"),
         }
     }
 
@@ -57,22 +61,25 @@ mod tests {
         // We can also pass `slice` without `&` so Cow owns it directly. In this
         // case no mutation occurs and thus also no clone, but the result is
         // still owned because it was never borrowed or mutated.
+        // 没有负数，不需要修改(不会触发to_mut()),预期结果保持 Cow::Owned(因为原始数据就是Owned)
         let slice = vec![0, 1, 2];
         let mut input = Cow::from(slice);
         match abs_all(&mut input) {
-            // TODO
+            Cow::Owned(_) => Ok(()),
+            _ => Err("Expected owned value"),
         }
     }
 
     #[test]
     fn owned_mutation() -> Result<(), &'static str> {
         // Of course this is also the case if a mutation does occur. In this
-        // case the call to `to_mut()` returns a reference to the same data as
-        // before.
+        // case the call to `to_mut()` returns a reference to the same data as before.
+        // 有负数，需要修改(会触发to_mut())，但因为数据已经是Owned，to_mut() 不会克隆，预期结果保持 Cow::Owned
         let slice = vec![-1, 0, 1];
         let mut input = Cow::from(slice);
         match abs_all(&mut input) {
-            // TODO
+            Cow::Owned(_) => Ok(()),
+            _ => Err("Expected owned value"),
         }
     }
 }
